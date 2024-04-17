0f287cefec9c601285956e4cc238d9c2aa3350cb
Maxim <max2morozov@gmail.com>
2024-04-17 23:30:09 +0300

    (dz_4) write code test_phone.py

---
+++ tests/test_phone.py
@@ -0,0 +1,33 @@
+import pytest
+from src.phone import Phone
+
+
+@pytest.fixture
+def phone():
+    phone = Phone("iPhone 14", 120_000, 5, 2)
+    return phone
+
+
+def test___init__(phone):
+    assert phone.name == "iPhone 14"
+    assert phone.price == 120_000
+    assert phone.quantity == 5
+    assert phone.number_of_sim == 2
+
+
+def test_repr():
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
+
+
+def test_str():
+    phone1 = Phone("iPhone 14", 120000, 5, 2)
+    assert str(phone1) == 'iPhone 14'
+
+
+@pytest.fixture
+def testing_data():
+    phone = Phone("iPhone 14", 120_000, 5, 2)
+    return testing_data
+
+
