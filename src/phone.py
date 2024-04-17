12c22196f8112d44e25897ebd406c0ece02a612c
Maxim <max2morozov@gmail.com>
2024-04-17 23:35:08 +0300

    hot fix (dz_4) write code phone.py

--- src/phone.py
+++ src/phone.py
@@ -2,31 +2,29 @@ from src.item import Item
 
 
 class Phone(Item):
-    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
-        """
-        Создание экземпляра класса Phone.
 
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        :param number_of_sim: количество поддерживаемых сим-карт
-        """
+    def __init__(self, name: str, price: float, quantity: int,
+                 number_of_sim: int) -> None:
         super().__init__(name, price, quantity)
         self.__number_of_sim = number_of_sim
+
+    def __repr__(self):
+        return (f"{self.__class__.__name__}('{self.name}', "
+                f"{self.price}, {self.quantity}, "
+                f"{self.number_of_sim})")
+
+    def __str__(self):
+        return f"{self.name}"
 
     @property
     def number_of_sim(self):
         return self.__number_of_sim
 
     @number_of_sim.setter
-    def number_of_sim(self, testing_data):
-        if int(testing_data) > 0:
-            self.__number_of_sim = int(testing_data)
+    def number_of_sim(self, sim):
+        if sim <= 0:
+            print(
+                'ValueError: Количество физических SIM-карт должно '
+                'быть целым числом больше нуля.')
         else:
-            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
-
-    def __str__(self) -> str:
-        return f'{self.name}'
+            self.__number_of_sim = sim
--- tests/test_phone.py
+++ tests/test_phone.py
@@ -1,33 +1,24 @@
 import pytest
+import coverage
+from src.item import Item
 from src.phone import Phone
 
 
 @pytest.fixture
-def phone():
-    phone = Phone("iPhone 14", 120_000, 5, 2)
-    return phone
-
-
-def test___init__(phone):
-    assert phone.name == "iPhone 14"
-    assert phone.price == 120_000
-    assert phone.quantity == 5
-    assert phone.number_of_sim == 2
-
-
-def test_repr():
-    phone1 = Phone("iPhone 14", 120_000, 5, 2)
-    assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
-
-
-def test_str():
-    phone1 = Phone("iPhone 14", 120000, 5, 2)
-    assert str(phone1) == 'iPhone 14'
+def test_item1():
+    return Item("Смартфон", 10000, 20)
 
 
 @pytest.fixture
-def testing_data():
-    phone = Phone("iPhone 14", 120_000, 5, 2)
-    return testing_data
+def test_phone1():
+    return Phone("iPhone 14", 120_000, 5, 2)
 
 
+def test_name(test_phone1):
+    assert str(test_phone1) == "iPhone 14"
+    assert repr(test_phone1) == "Phone('iPhone 14', 120000, 5, 2)"
+    assert test_phone1.number_of_sim == 2
+
+
+def test_add(test_item1, test_phone1):
+    assert test_phone1 + test_item1 == 25
